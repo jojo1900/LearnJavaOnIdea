dead83f032f8849c3466bd8e96bd442dbd6b8375
jojo <zbhdaohuoxian>
2020-04-24 16:32:38 +0800

    PcTest

--- src/Employee.java
+++ src/Employee.java
@@ -5,26 +5,26 @@ public class Employee {
     private double salary;
     private LocalDate hireDate;
 
-    public Employee(String n,double s,int year,int month,int day){
+    public Employee(String n, double s, int year, int month, int day) {
         name = n;
         salary = s;
-        hireDate = LocalDate.of(year,month,day);
+        hireDate = LocalDate.of(year, month, day);
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public double getSalary(){
+    public double getSalary() {
         return salary;
     }
 
-    public LocalDate getHireDate(){
+    public LocalDate getHireDate() {
         return hireDate;
     }
 
-    public void raiseSalary(double percent){
-        double raise = salary*percent/100;
+    public void raiseSalary(double percent) {
+        double raise = salary * percent / 100;
         salary += raise;
     }
 }
